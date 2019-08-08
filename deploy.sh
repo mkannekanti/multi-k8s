346bf6ffde41e8086bf64188ed44b1f1e4da79de
Mohan Kumar Kannekanti <mohan.kannekanti@gmail.com>
2019-08-08 16:58:49 -0700

    Fixing deploy.sh file errors

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 docker build -t mkannekanti/multi-client:latest -t mkannekanti/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t mkannekanti/multi-server:latest -t mkannekanti/multi-server:$SHA -f ./server/Dockerfile ./server
 docker built -t mkannekanti/multi-worker:latest -t mkannekanti/multi-worker:$SHA -f ./worker/Dockerfile ./worker
@@ -11,7 +10,7 @@ docker push mkannekanti/multi-client:$SHA
 docker push mkannekanti/multi-server:$SHA
 docker push mkannekanti/multi-worker:$SHA
 
-kubectl apply -f ./k8s
+kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=mkannekanti/multi-server:$SHA
 kubectl set image deployments/client-deployment client=mkannekanti/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=mkannekanti/multi-worker:$SHA
